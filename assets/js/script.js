f29edea49b32518f08dc26111231bf4aceb2bcb3
george fisher <georgezappa@gmail.com>
2022-12-12 19:42:36 -0600

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+import menu from './module/menu.js';
+import animations from './module/animations.js';
+import smoothScroll from './module/smoothScroll.js';
+import themeToggle from './module/theme.js';
+/* import adBlockChecker from './module/adblock.js'; */
+
+
+menu();
+smoothScroll();
+animations();
+themeToggle();
+/* adBlockChecker(); */
